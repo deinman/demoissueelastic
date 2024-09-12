41a5882353eb3a33e234adb17798b6c18a9596ea
Daniel Inman <inman.daniel.e@gmail.com>
2024-09-12 15:58:41 -0400

    Add demo projects.

---
+++ DemoIssueElasticNest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Nest.JsonNetSerializer;
+
+Console.WriteLine("Hello, World!");
+return;
+
+void DefaultJsonNetSerializer()
+{
+    var pool = new SingleNodePool(new Uri("http://localhost:9200"));
+    var connectionSettings =
+        new ElasticsearchClientSettings(pool, sourceSerializer: JsonNetSerializer.Default);
+    var client = new ElasticsearchClient(connectionSettings);
+}
+
+void DefaultJsonNetSerializerUnsugared()
+{
+    var pool = new SingleNodePool(new Uri("http://localhost:9200"));
+    var connectionSettings = new ElasticsearchClientSettings(
+        pool,
+        sourceSerializer: (builtin, settings) => new JsonNetSerializer(builtin, settings));
+    var client = new ElasticsearchClient(connectionSettings);
+}
